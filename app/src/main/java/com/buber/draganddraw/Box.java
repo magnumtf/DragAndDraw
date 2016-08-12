48254df1ed663051b442e790e6883d0a3693b951
t1004800 <magnumtf@yahoo.com>
2016-08-12 10:53:23 -0400

    Initial Version.

---
+++ app/src/main/java/com/buber/draganddraw/Box.java
@@ -0,0 +1,28 @@
+package com.buber.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by t1004800 on 8/4/2016.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+}
